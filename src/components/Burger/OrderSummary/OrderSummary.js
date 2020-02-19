99d15f4d851b5d0724428581f78af41717fd775e
Subeshsingh <subeshbikkisingh@gmail.com>
2020-02-19 18:24:06 +0530

    Removing log and after cleaning a bit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,8 +5,8 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
     // This could be a functional component, doesn't have to be a class
-    componentWillUpdate() {
-        console.log('[OrderSummary] WillUpdate');
+    UNSAFE_componentWillUpdate() {
+       // console.log('[OrderSummary] WillUpdate');
     }
 
     render () {
